e23285ba0b7555715f4076b49a5640524a39dd28
Jefferson143 <jeffersono.escobare@gmail.com>
2025-05-30 15:15:11 -0600

    Trabajo clase 9

--- Clase_9/6.2.1_eliminando_valores_duplicados.py
+++ Clase_9/6.2.1_eliminando_valores_duplicados.py
@@ -1,3 +1,14 @@
+'''
+Clase:        Clase 9
+Tema:         Listas
+Ejercicio:    6.2.1 eliminando valores duplicados
+Descripción:  Dada una lista ingresada por el usuario, elimina los elementos duplicados
+manteniendo la primera aparición de cada número.
+
+Autor:        Jefferson Omar Escobar Estupinian
+Fecha:        2025-05-30
+Estado:       [ Terminado ]
+'''
 lista = list(map(int, input("Dame la lista de tus numeros para eliminar los duplicados ").split()))
 duplicados = [ ]
 for numero in lista:
